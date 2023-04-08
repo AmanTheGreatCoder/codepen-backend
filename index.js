e1872c8f8f1ec68edf394fae82c7e41586d62533
AmanTheGreatCoder <amanwadhwaniya159@gmail.com>
2023-04-08 12:37:49 +0530

    Hi

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ require('dotenv').config();
 app.use(express.json())
 app.use(cors())
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
     res.json('Hello world')
